3ea3043f79f56cb6796a439195e19e5fc9846948
Mehul Nandaniya <220130107062@gecg28.ac.in>
2025-02-02 16:47:03 +0530

    Number Guessing Game completed

---
+++ NumGuess.cs
@@ -0,0 +1,45 @@
+﻿namespace CodeX
+{
+    class NumGuess
+    {
+        static void Main(string[] args)
+        {
+            bool isCorrectGuess = false;
+            Random r = new Random();
+
+            int randomNum = r.Next(1, 111);
+
+            Console.WriteLine("Welcome to the number guessing game!");
+            Console.WriteLine("A number between 1 and 100 will be generated.");
+            Console.WriteLine("If you guess the correct number you win!");
+
+            while (!isCorrectGuess)
+            {
+                Console.WriteLine("Please enter your guess :");
+                int guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess < randomNum)
+                {
+                    Console.WriteLine("Your Guess Is Too Low");
+                }
+                else if (guess > randomNum)
+                {
+                    Console.WriteLine("Your Guess Is Too High");
+                }
+                else
+                {
+                    Console.WriteLine("Correct!");
+                    isCorrectGuess = true;
+                }
+            }
+            Console.WriteLine("Congratulations ,You Have won the Game, The Number Was" + randomNum);
+
+
+
+        }
+
+
+
+
+    }
+}
